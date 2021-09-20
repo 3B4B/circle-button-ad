ad603d377bb2a8f98b11708b3b55eba8207b0987
dronk6 <dkr5301@psu.edu>
2021-09-20 01:07:15 -0400

    Played around with button and started working on CSS

--- src/CircleButtonAd.js
+++ src/CircleButtonAd.js
@@ -8,30 +8,58 @@ export class CircleButtonAd extends LitElement {
         padding: 25px;
         color: var(--circle-button-ad-text-color, #000);
       }
+      button {
+        min-width: 60px;
+        max-width: 100%;
+        min-height: 60px;
+        max-height: auto;
+        text-decoration: none;
+        display: inline-block;
+        box-shadow: 0px 0px 4px #202224;
+        border: 10px solid transparent;
+        color: whitesmoke;
+        background-color: #202224;
+        font: helvetica;
+        font-size: 60px;
+        font-weight: bold;
+        border-radius: 50%;
+        position: relative;
+        text-align: center;
+        vertical-align: middle;
+        padding: 15px;
+      }
+      button:before {
+        content: '';
+        display: inline-block;
+        vertical-align: middle;
+        padding-top: 100%;
+      }
+      button:hover {
+        color: #202224;
+        background-color: whitesmoke;
+        border: 10px solid #202224;
+      }
     `;
   }
 
   static get properties() {
     return {
-      title: { type: String },
-      counter: { type: Number },
+      buttonText: { type: String },
+      buttonLink: { type: String },
     };
   }
 
   constructor() {
     super();
-    this.title = 'Hey there';
-    this.counter = 5;
-  }
-
-  __increment() {
-    this.counter += 1;
+    this.buttonText = 'Google';
+    this.buttonLink = 'https://google.com';
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <a href=${this.buttonLink} rel="noreferrer" target="_blank" role="button"
+        ><button>${this.buttonText}</button></a
+      >
     `;
   }
 }
