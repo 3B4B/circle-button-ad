45954ebe62dcbe4047e147e5cbe264150fe149cd
dronk6 <dkr5301@psu.edu>
2021-09-23 14:17:07 -0400

    Added icon to code and added slot tags for more index.html customizability

--- src/CircleButtonAd.js
+++ src/CircleButtonAd.js
@@ -2,6 +2,8 @@ import { html, css, LitElement } from 'lit';
 // import { remoteLinkBehavior } from "@lrnwebcomponents/utils/remoteLinkBehavior.js";
 // import { activeStateBehavior } from "@lrnwebcomponents/utils/activeStateBehavior.js";
 // import { SimpleColors } from "@lrnwebcomponents/simple-colors/simple-colors.js";
+import '@lrnwebcomponents/simple-icon/lib/simple-icon-lite.js';
+import '@lrnwebcomponents/simple-icon/lib/simple-icons.js';
 
 export class CircleButtonAd extends LitElement {
   static get styles() {
@@ -9,38 +11,37 @@ export class CircleButtonAd extends LitElement {
       :host {
         display: block;
         padding: 25px;
-        color: var(--circle-button-ad-text-color, #000);
-      }
-      button {
-      }
-      button:active {
-      }
-      button:hover {
-      }
-      button:focus {
-      }
-      button:disabled {
+        --circle-button-ad-color: #202224;
+        --circle-button-ad-text-color: whitesmoke;
       }
 
-      a button {
-        min-width: 60px;
+      button :active {
+      }
+      button :disabled {
+        opacity: 0.4;
+      }
+
+      button {
+        min-width: 120px;
         max-width: 100%;
-        min-height: 60px;
+        min-height: 120px;
         max-height: auto;
         text-decoration: none;
         display: inline-block;
-        box-shadow: 0px 0px 4px #202224;
+        box-shadow: 0px 0px 4px var(--circle-button-ad-color);
         border: 10px solid transparent;
-        color: whitesmoke;
-        background-color: #202224;
+        color: var(--circle-button-ad-text-color);
+        background-color: var(--circle-button-ad-color);
         font: helvetica;
-        font-size: 60px;
+        font-size: 2rem;
         font-weight: bold;
+        overflow-wrap: break-word;
         border-radius: 50%;
         position: relative;
         text-align: center;
         vertical-align: middle;
         padding: 15px;
+        transition: 0.5s;
       }
       button:before {
         content: '';
@@ -48,30 +49,37 @@ export class CircleButtonAd extends LitElement {
         vertical-align: middle;
         padding-top: 100%;
       }
-      button:hover {
-        color: #202224;
-        background-color: whitesmoke;
-        border: 10px solid #202224;
+      button:hover,
+      :focus {
+        color: var(--circle-button-ad-color);
+        background-color: var(--circle-button-ad-text-color);
+        border: 10px solid var(--circle-button-ad-color);
+      }
+      a {
       }
     `;
   }
 
   static get properties() {
     return {
-      buttonText: { type: String },
-      buttonLink: { type: String },
+      text: { type: String, attribute: 'text' },
+      link: { type: String, attribute: 'link' },
+      icon: { type: String, attribute: 'icon' },
+      disabled: { type: Boolean, reflect: true },
     };
   }
 
   constructor() {
     super();
-    this.buttonText = 'Google';
-    this.buttonLink = 'https://google.com';
+    this.text = 'Google';
+    this.link = 'https://google.com';
+    this.icon = 'editor:bubble-chart';
+    this.disabled = false;
 
     // TODO: From simple-cta, want to see if this works
     if (this.querySelector('a')) {
       this.link = this.querySelector('a').getAttribute('href');
-      this.title = this.querySelector('a').innerText;
+      this.text = this.querySelector('a').innerText;
       this.innerHTML = null;
     }
   }
@@ -87,11 +95,22 @@ export class CircleButtonAd extends LitElement {
 
   render() {
     return html`
-      <a href=${this.buttonLink} rel="noreferrer" target="_blank" role="button"
-        ><button>${this.buttonText}</button></a
+      <a
+        href=${this.link}
+        rel="noreferrer"
+        target="_blank"
+        role="button"
+        tabindex="-1"
       >
+        <button>
+          <slot>${this.text}</slot>
+          ${this.icon
+            ? html`<simple-icon-lite icon=${this.icon}></simple-icon-lite>`
+            : ''}
+        </button>
+      </a>
     `;
   }
 }
 
-// add button, button hover, button focus, button disabled, buton active,
+// add button focus, button disabled, buton active,
